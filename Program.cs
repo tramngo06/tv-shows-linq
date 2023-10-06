84c0b193634ca94c563533acd7cf28d025337fe0
Jaime Spacco <jspacco@knox.edu>
2023-10-06 03:12:48 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+// name of TV show
+//string show = "shows/buffy.json";
+//string show = "shows/steven-universe.json";
+//string show = "shows/mandalorian.json";
+string show = "shows/mash.json";
+//string show = "shows/stranger-things.json";
+
+var options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true
+};
+
+string text = File.ReadAllText(show);
+var shows = JsonSerializer.Deserialize<List<Show>>(text, options);
+
+
+shows.Select(show => show.Name)
+    .Where(title => title.Contains("Pilot"))
+    .ToList()
+    .ForEach(Console.WriteLine);
---
+++ Show.cs
@@ -0,0 +1,14 @@
+class Show
+{
+    public int Id {get; set;}
+    public string Url {get; set;}
+    public string Name {get; set;}
+    public int Season {get; set;}
+    public int Number {get; set;}
+    public string Type {get; set;}
+    public string Airdate {get; set;}
+    public string Airtime {get; set;}
+    public int Runtime {get; set;}
+    public string Summary {get; set;}
+    //public Dictionary<string, double> Rating {get; set;}
+}
